dc04abe3c87dd4a22344548b82dabef357996d2c
lshy1993 <lshy1993@live.com>
2015-03-13 21:47:55 +0900

    v0.5.3

--- KanDic/Viewer/Map.xaml.cs
+++ KanDic/Viewer/Map.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using KanDic.Resources;
+using System.Xml;
+using System.Reflection;
 
 namespace KanDic.Viewer
 {
@@ -21,12 +24,17 @@ namespace KanDic.Viewer
     /// </summary>
     public partial class Map : UserControl
     {
-        public class MapDetail
+        public MapInfo[] MapPoint = new MapInfo[25];
+        public int num;
+
+        #region xaml显示图片用临时class
+        public class MapImage
         {
             public string MAP { set; get; }
-            public MapDetail() { }
-            public MapDetail(string x) { MAP = x; }
+            public MapImage() { }
+            public MapImage(string x) { MAP = x; }
         }
+        #endregion
 
         public int radionum,buttonnum;
 
@@ -34,6 +42,8 @@ namespace KanDic.Viewer
         {
             InitializeComponent();
         }
+
+        #region 选择关卡标签
 
         private void RadioButton_Click(object sender, RoutedEventArgs e)
         {
@@ -85,51 +95,129 @@ namespace KanDic.Viewer
             }
             
         }
+        #endregion
 
+        #region 打开地图
         private void bt_Click(object sender, RoutedEventArgs e)
         {
             Button xx = sender as Button;
             buttonnum = Convert.ToInt32(xx.Tag);
-            MapBackground.DataContext = new MapDetail("/Cache/map/"+radionum+"-"+buttonnum+".png");
+            MapBackground.DataContext = new MapImage("/Cache/map/"+radionum+"-"+buttonnum+".png");
             Button_Init(radionum+"-"+buttonnum);
             Map_Show();
         }
+        #endregion
 
+        #region 读取xml生成数据，绘制按钮
         private void Button_Init(string x)
         {
-            switch (x)
+            System.Reflection.Assembly _assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            System.IO.Stream sStream = _assembly.GetManifestResourceStream("KanDic.Resources.Data.Map.xml");
+            XmlDocument MapList = new XmlDocument();
+            MapList.Load(sStream);
+            foreach (XmlNode temp in MapList.DocumentElement.ChildNodes)
             {
-                default :
-                    TextBlock xx = new TextBlock();
-                    xx.Text = "数据制作中……";
-                    xx.FontSize = 40;
-                    Canvas.SetTop(xx, 180);
-                    Canvas.SetLeft(xx, 280);
-                    MapButton.Children.Add(xx);
-                    TextBlock yy = new TextBlock();
-                    yy.Text = "Now Loading......";
-                    yy.FontSize = 40;
-                    Canvas.SetTop(yy, 220);
-                    Canvas.SetLeft(yy, 280);
-                    MapButton.Children.Add(yy);
+                if (temp.FirstChild.InnerText == x)
+                {
+                    num = 0; 
+                    foreach (XmlNode y in temp.ChildNodes)
+                    {
+                        if(y.Name != "Key")Add_Point(y);
+                    }
                     break;
+                }
+                
+            }
+            MapButton.Children.Clear();
+            ResetPos();
+            for (int i = 1; i <= num; i++)
+            {
+                RadioButton xx = new RadioButton();
+                xx.Style = (Style)FindResource(MapPoint[i].Type);
+                xx.Tag = i;
+                MapButton.Children.Add(xx);
+                Canvas.SetLeft(xx, Convert.ToDouble(MapPoint[i].xPos));
+                Canvas.SetTop(xx, Convert.ToDouble(MapPoint[i].yPos));
+                xx.Checked += new RoutedEventHandler(Point_Detail);
             }
         }
+        #endregion
 
+        #region 生成MapInfo类
+        private void Add_Point(XmlNode x)
+        {
+            num++;
+            MapPoint[num] = new MapInfo();
+            foreach (XmlNode yy in x.ChildNodes)
+            {
+                string name1 = yy.Name;
+                typeof(MapInfo).GetProperty(name1).SetValue(MapPoint[num], yy.InnerText, null);
+            }
+        }
+        #endregion
+
+        #region 打开地图动画
         private void Map_Show()
         {
             MapBox.Visibility = Visibility.Visible;
             DoubleAnimation da = new DoubleAnimation();
             da.From = 480;
             da.To = 0;
-            da.Duration = TimeSpan.FromSeconds(0.3);
+            da.Duration = TimeSpan.FromSeconds(0.2);
             MapBackground.BeginAnimation(Canvas.TopProperty, da);
         }
+        #endregion
+
+        #region 打开地图点介绍
+        private void Point_Detail(object sender, RoutedEventArgs e)
+        {
+            RadioButton xx = (RadioButton)sender;
+            DoubleAnimation da = new DoubleAnimation();
+            int tagnum = Convert.ToInt32(xx.Tag);
+            bool isbattle = true;
+            PointDetail.DataContext = MapPoint[tagnum];
+            if (MapPoint[tagnum].nobattle != null) { isbattle = false; }
+            ResetPos();
+            if (Canvas.GetLeft(xx) < 380)
+            {
+                da.From = 800;
+                da.To = 500;
+            }
+            else
+            {
+                da.From = -305;
+                da.To = 0;
+            }
+            da.Duration = TimeSpan.FromSeconds(0.2);
+            if (isbattle)
+            {
+                Battle.BeginAnimation(Canvas.LeftProperty, da);
+            }
+            else
+            {
+                NoBattle.BeginAnimation(Canvas.LeftProperty, da);
+            }
+        }
+        #endregion
+
+        #region 介绍Panel复原
+        private void ResetPos()
+        {
+            Battle.BeginAnimation(Canvas.LeftProperty, null);
+            NoBattle.BeginAnimation(Canvas.LeftProperty, null);
+            Battle.SetValue(Canvas.LeftProperty, (double)-305);
+            NoBattle.SetValue(Canvas.LeftProperty, (double)-305);
+        }
+        #endregion
 
         private void MapBox_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             MapBox.Visibility = Visibility.Hidden;
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResetPos();
+        }
     }
 }
